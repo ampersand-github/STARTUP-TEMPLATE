bcb80812f233a02c6465acd8022ba59c8989e936
ampersand-github <ampersand.dev@gmail.com>
2022-04-26 23:29:52 +0900

    ぷｓｈ

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div>Hello World2</div>
+  return <div>Hello World3</div>
 };
 
 export default Home;
