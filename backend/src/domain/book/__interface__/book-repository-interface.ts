2ce74e6b9a7ce5f87cde378e4e52ca7dd841f1b3
ampersand-github <ampersand.dev@gmail.com>
2022-05-13 10:07:12 +0900

    修正

--- backend/src/domain/book/__interface__/book-repository-interface.ts
+++ backend/src/domain/book/__interface__/book-repository-interface.ts
@@ -1,10 +1,10 @@
 import { Book } from 'src/domain/book/book';
-import { BookId } from 'src/domain/book/book-id';
+import { BookId } from 'src/domain/book/book-id/book-id';
 
 export interface IBookRepository {
   findAll(): Promise<Book[]>;
   findOne(id: BookId): Promise<Book | null>;
-  // register(entity: Book): Promise<void>;
-  // update(entity: Book): Promise<void>;
+  // register(entity: Borrow): Promise<void>;
+  // update(entity: Borrow): Promise<void>;
   // delete(id: BookId): Promise<void>;
 }
