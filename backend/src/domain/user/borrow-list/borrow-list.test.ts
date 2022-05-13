6f8b04f94bf158946c8f1d07632ae64d44d7f9fb
ampersand-github <ampersand.dev@gmail.com>
2022-05-14 00:58:14 +0900

    貸出

---
+++ backend/src/domain/user/borrow-list/borrow-list.test.ts
@@ -0,0 +1,71 @@
+import { BorrowingList } from './borrow-list';
+import { Borrow } from '../../book/borrow/borrow';
+import { BookId } from '../../book/book-id/book-id';
+import { UserId } from '../user-id/user-id';
+
+describe('BorrowList', () => {
+  const borrow1 = new Borrow({
+    bookId: BookId.create(),
+    userId: UserId.create(),
+    startAt: new Date(),
+  });
+  const borrow2 = new Borrow({
+    bookId: BookId.create(),
+    userId: UserId.create(),
+    startAt: new Date(),
+    endAt: new Date(),
+  });
+  const borrow3 = new Borrow({
+    bookId: BookId.create(),
+    userId: UserId.create(),
+    startAt: new Date(),
+  });
+  const borrow4 = new Borrow({
+    bookId: BookId.create(),
+    userId: UserId.create(),
+    startAt: new Date(),
+  });
+  const borrow5 = new Borrow({
+    bookId: BookId.create(),
+    userId: UserId.create(),
+    startAt: new Date(),
+  });
+  const borrow6 = new Borrow({
+    bookId: BookId.create(),
+    userId: UserId.create(),
+    startAt: new Date(),
+  });
+
+  describe('constructor()', () => {
+    it('オブジェクトの生成ができる', () => {
+      const borrowList = new BorrowingList({
+        borrowList: [borrow1, borrow2, borrow3],
+      });
+      expect(borrowList).toEqual(expect.any(BorrowingList));
+    });
+    it('5冊のレンタルができる', () => {
+      const borrowList = new BorrowingList({
+        borrowList: [borrow1, borrow2, borrow3, borrow4, borrow5],
+      });
+      expect(borrowList).toEqual(expect.any(BorrowingList));
+    });
+    it('6冊のレンタルはできない', () => {
+      const actual = () => {
+        new BorrowingList({
+          borrowList: [borrow1, borrow2, borrow3, borrow4, borrow5, borrow6],
+        });
+      };
+      expect(actual).toThrowError('貸出は一人5冊までです');
+    });
+  });
+
+  describe('getCollection', () => {
+    const borrowList = new BorrowingList({
+      borrowList: [borrow1, borrow2, borrow3],
+    });
+    const collection = borrowList.getCollection();
+    expect(collection).toEqual(
+      expect.arrayContaining([borrow1, borrow2, borrow3]),
+    );
+  });
+});
