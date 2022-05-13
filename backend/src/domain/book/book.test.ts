734985b0133ccbb669e048fdf77349a183732c9a
ampersand-github <ampersand.dev@gmail.com>
2022-05-14 00:58:29 +0900

    書籍

---
+++ backend/src/domain/book/book.test.ts
@@ -0,0 +1,52 @@
+import { TAG, Tag } from './tag/tag';
+import { BookId } from './book-id/book-id';
+import { Book, IBook } from './book';
+import { TagList } from './tag/tag-list';
+import { Borrow } from './borrow/borrow';
+import { UserId } from '../user/user-id/user-id';
+
+describe('Book', () => {
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  // tag
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  const tag1 = new Tag({ name: TAG.design });
+  const tag2 = new Tag({ name: TAG.ui });
+  const tag3 = new Tag({ name: TAG.ops });
+  const tagList = new TagList({ tagsList: [tag1, tag2, tag3] });
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  // borrow
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  const borrow = new Borrow({
+    bookId: BookId.create(),
+    userId: UserId.create(),
+    startAt: new Date(),
+    endAt: new Date(),
+  });
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  // props
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  const props: IBook = {
+    name: 'book1',
+    author: 'author',
+    tagList: tagList,
+    isLost: false,
+    isPrivate: false,
+    latestBorrow: borrow,
+  };
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  // 以下テスト
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  describe('constructor', () => {
+    it('createできる', () => {
+      const book = Book.create(props);
+      expect(book).toEqual(expect.any(Book));
+    });
+    it('reBuildできる', () => {
+      const bookId = BookId.reBuild('43145f95-2034-4fae-b88f-ca0bdf7890bd');
+      const book = Book.reBuild(props, bookId);
+      expect(book).toEqual(expect.any(Book));
+      expect(book.id).toStrictEqual(bookId);
+    });
+  });
+});
--- backend/src/domain/book/book.ts
+++ backend/src/domain/book/book.ts
@@ -1,30 +1,34 @@
 import { AggregateRoot } from 'src/domain/__shared__/aggregate-root';
-import { TagList } from './tag-list';
-import { BookId } from './book-id';
+import { TagList } from './tag/tag-list';
+import { BookId } from './book-id/book-id';
+import { Borrow } from './borrow/borrow';
 
 export interface IBook {
   name: string;
-  tagList: TagList;
   author: string;
-  rating: number; // todo あとで値オブジェクト化
+  tagList: TagList;
+  isLost: boolean;
+  isPrivate: boolean;
+  latestBorrow: Borrow;
 }
 
 export class Book extends AggregateRoot<IBook, BookId> {
-  public get name(): string {
-    return this.props.name;
-  }
+  private readonly name: string;
+  private readonly author: string;
+  private readonly tagList: TagList;
+  private readonly isLost: boolean;
+  private readonly isPrivate: boolean;
+  private readonly latestBorrow: Borrow;
 
-  public get tagList(): string[] {
-    return this.props.tagList.values;
+  private constructor(props: IBook, id: BookId) {
+    super(props, id);
+    this.name = props.name;
+    this.author = props.author;
+    this.tagList = props.tagList;
+    this.isLost = props.isLost;
+    this.isPrivate = props.isPrivate;
+    this.latestBorrow = props.latestBorrow;
   }
-  public get author(): string {
-    return this.props.author;
-  }
-
-  public get rating(): number {
-    return this.props.rating;
-  }
-
 
   public static create(props: IBook): Book {
     return new Book(props, BookId.create());
