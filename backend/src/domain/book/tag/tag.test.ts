31027bf5b8cacf70317862820b458e2a5bf861ce
ampersand-github <ampersand.dev@gmail.com>
2022-05-13 10:24:47 +0900

    tag修正

--- backend/src/domain/book/tag/tag.test.ts
+++ backend/src/domain/book/tag/tag.test.ts
@@ -1,24 +1,25 @@
 import { ITag, TAG, Tag } from 'src/domain/book/tag/tag';
 
 describe('Tag', () => {
-  const props: ITag = { name: TAG.tech };
 
   describe('constructor', () => {
     it('オブジェクトの生成ができる', () => {
+      const props: ITag = { name: TAG.ui };
       const tag = new Tag(props);
       expect(tag).toEqual(expect.any(Tag));
     });
     it('存在しないタグを生成しようとするとエラーになる', () => {
       const props: ITag = { name: 'aaa' };
-      const result = () => new Tag(props);
-      expect(result).toThrowError('タグが存在しません');
+      const actual = () => new Tag(props);
+      expect(actual).toThrowError('タグが存在しません');
     });
   });
 
   describe('get', () => {
-    const tag = new Tag(props);
     it('値の取得ができる', () => {
-      expect(tag.value).toEqual(props.name);
+      const props: ITag = { name: TAG.ui };
+      const tag = new Tag(props);
+      expect(tag.getValue()).toEqual(props.name);
     });
   });
 });
--- backend/src/domain/book/tag/tag.ts
+++ backend/src/domain/book/tag/tag.ts
@@ -1,14 +1,14 @@
 import { ValueObject } from 'src/domain/__shared__/value-object';
 
 export const TAG = {
-  design:"設計",
-  ui:"デザイン",
-  ts:"TypeScript",
-  ops:"運用",
-  java:"Java",
-  attitude: "心構え",
-  html :"HTML",
-  go:"Go"
+  design: '設計',
+  ui: 'デザイン',
+  ts: 'TypeScript',
+  ops: '運用',
+  java: 'Java',
+  attitude: '心構え',
+  html: 'HTML',
+  go: 'Go',
 } as const;
 
 export type tagType = typeof TAG[keyof typeof TAG];
@@ -18,8 +18,10 @@ export interface ITag {
 }
 
 export class Tag extends ValueObject<ITag> {
-  public get value() {
-    return this.props.name;
+  private readonly value:string
+
+  public getValue() {
+   return this.value
   }
 
   public constructor(props: ITag) {
@@ -28,5 +30,6 @@ export class Tag extends ValueObject<ITag> {
     if (!tagList.find((plan: tagType) => plan === props.name)) {
       throw new Error(`${props.name}のタグが存在しません`);
     }
+    this.value = props.name
   }
 }
