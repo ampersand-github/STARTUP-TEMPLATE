ecdce90e3aac401702744d7986d538c2b92ff3e8
ampersand-github <ampersand.dev@gmail.com>
2022-05-21 13:55:54 +0900

    ユーザー集約修正

--- backend/src/domain/user/user.ts
+++ backend/src/domain/user/user.ts
@@ -1,28 +1,20 @@
 import { UserId } from 'src/domain/user/user-id/user-id';
 import { AggregateRoot } from 'src/domain/__shared__/aggregate-root';
-import { BorrowingList } from './borrow-list/borrow-list';
 
 export interface IUser {
   name: string;
-  borrowingList: BorrowingList;
 }
 
 export class User extends AggregateRoot<IUser, UserId> {
   private readonly name: string;
-  private readonly borrowingList: BorrowingList;
 
   public getName() {
     return this.name;
   }
 
-  public getBorrowingList() {
-    return this.borrowingList;
-  }
-
   private constructor(props: IUser, id: UserId) {
     super(props, id);
     this.name = props.name;
-    this.borrowingList = props.borrowingList;
   }
 
   public static create(props: IUser): User {
