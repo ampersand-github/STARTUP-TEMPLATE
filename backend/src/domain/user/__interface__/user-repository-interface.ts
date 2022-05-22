062276572592bea48c03ca4bef6e87ef83a6e7bb
ampersand-github <ampersand.dev@gmail.com>
2022-05-22 12:17:46 +0900

    user-repository修正

--- backend/src/domain/user/__interface__/user-repository-interface.ts
+++ backend/src/domain/user/__interface__/user-repository-interface.ts
@@ -2,7 +2,7 @@ import { User } from '../user';
 import { UserId } from '../user-id/user-id';
 
 export interface IUserRepository {
-  findAll(): Promise<User[]>;
+  //findAll(): Promise<User[]>;
   findOne(id: UserId): Promise<User | null>;
   save(entity: User): Promise<void>;
 }
