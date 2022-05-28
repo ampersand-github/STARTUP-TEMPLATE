e73ee431ba2feb976ad360bd9f63401ff9767657
ampersand-github <ampersand.dev@gmail.com>
2022-05-28 14:17:48 +0900

    syuusei

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,6 +12,9 @@ async function bootstrap() {
   firebaseInit();
   const port = Number(configService.get('BACKEND_PORT')) || 3001; // Cloud Run の要件。環境変数PORTで起動するように。
   await app.listen(port, '0.0.0.0'); // '0.0.0.0' を追加して外部からのアクセスを受け入れる。
+  //
+  console.log(`環境:${process.env.NODE_ENV}`)
+  console.log(`port:${port.toFixed()}`)
 }
 
 bootstrap().catch((e) => console.log(e));
---
+++ backend/src/presentation/controller/pass/index.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get } from '@nestjs/common';
+import { CustomLoggerService } from '../../../__shared__/logger/custom-logger.service';
+import {PrismaService} from "../../../infrastructure/prisma/prisma.service";
+
+@Controller('pass')
+export class PassController {
+    public constructor(
+        private readonly prismaService: PrismaService,
+        private readonly logger: CustomLoggerService,
+    ) {}
+
+    @Get('1')
+    //  curl http://localhost:3001/book
+    public async pass() {
+        return 'passed!';
+    }
+
+    /*
+      @Get()
+    //  curl http://localhost:3001/book
+    public async getHello() {
+      const allBooks = await findAllBook(this.bookRepository);
+      return findAllBooksDto(allBooks);
+    }
+     */
+}
