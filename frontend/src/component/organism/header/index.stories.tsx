de4118dde1042791e8418a09ac433c17ee7f1d43
ampersand-github <ampersand.dev@gmail.com>
2022-05-29 19:30:46 +0900

    header

--- frontend/src/component/organism/header/index.stories.tsx
+++ frontend/src/component/organism/header/index.stories.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Header } from './index';
+import React from 'react';
+
 
 export default {
   title: 'Organism/Header',
   component: Header,
 } as ComponentMeta<typeof Header>;
 
-const TemplateStory: ComponentStory<typeof Header> = () => <Header />;
 
-export const Default = TemplateStory.bind({});
+const TemplateStory: ComponentStory<typeof Header> = (args) => <Header  {...args} />;
+
+export const SignInEd = TemplateStory.bind({});
+SignInEd.args = {
+  isAuth:true,
+  signInUrl:"string",
+  signUpUrl:"string",
+  onSignOut: new Promise(() => {})
+
+}
+
+export const NotSignIn = TemplateStory.bind({});
+NotSignIn.args = {
+  isAuth:false,
+  signInUrl:"string",
+  signUpUrl:"string",
+  onSignOut: new Promise(() => {})
+
+}
