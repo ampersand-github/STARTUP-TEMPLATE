720ad90a16ac1ca825cc568efad2038160a75065
ampersand-github <ampersand.dev@gmail.com>
2022-05-29 19:34:48 +0900

    sotryを作った

--- frontend/src/component/organism/header/index.stories.tsx
+++ frontend/src/component/organism/header/index.stories.tsx
@@ -2,29 +2,27 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Header } from './index';
 import React from 'react';
 
-
 export default {
   title: 'Organism/Header',
   component: Header,
 } as ComponentMeta<typeof Header>;
 
-
-const TemplateStory: ComponentStory<typeof Header> = (args) => <Header  {...args} />;
+const TemplateStory: ComponentStory<typeof Header> = (args) => (
+  <Header {...args} />
+);
 
 export const SignInEd = TemplateStory.bind({});
 SignInEd.args = {
-  isAuth:true,
-  signInUrl:"string",
-  signUpUrl:"string",
-  onSignOut: new Promise(() => {})
-
-}
+  isAuth: true,
+  signInUrl: 'string',
+  signUpUrl: 'string',
+  onSignOut: new Promise(() => {}),
+};
 
 export const NotSignIn = TemplateStory.bind({});
 NotSignIn.args = {
-  isAuth:false,
-  signInUrl:"string",
-  signUpUrl:"string",
-  onSignOut: new Promise(() => {})
-
-}
+  isAuth: false,
+  signInUrl: 'string',
+  signUpUrl: 'string',
+  onSignOut: new Promise(() => {}),
+};
--- frontend/src/component/organism/header/index.tsx
+++ frontend/src/component/organism/header/index.tsx
@@ -5,29 +5,31 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import AppBar from '@mui/material/AppBar';
 import { Box } from '@mui/material';
-import {CustomLinkButton} from "../../atom/custom-link-button";
-import {CustomButton} from "../../atom/custom-button";
+import { CustomLinkButton } from '../../atom/custom-link-button';
+import { CustomButton } from '../../atom/custom-button';
 
 export interface IHeader {
-    isAuth:boolean, // 認証済みか
-    signInUrl:string,
-    signUpUrl:string,
-    onSignOut: Promise<void>
+  isAuth: boolean; // 認証済みか
+  signInUrl: string;
+  signUpUrl: string;
+  onSignOut: Promise<void>;
 }
 
-export const Header = (props:IHeader): JSX.Element => {
+export const Header = (props: IHeader): JSX.Element => {
   return (
     <AppBar position="static" component={'header'}>
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           書籍レンタルシステム
         </Typography>
-        {props.isAuth
-            ? <CustomButton text={"サインアウト"} onClick={props.onSignOut} />
-            : <>
-                <CustomLinkButton text={"ログイン"} link={props.signInUrl}/>
-                <CustomLinkButton text={"新規登録"} link={props.signUpUrl}/>
-            </>}
+        {props.isAuth ? (
+          <CustomButton text={'サインアウト'} onClick={props.onSignOut} />
+        ) : (
+          <>
+            <CustomLinkButton text={'ログイン'} link={props.signInUrl} />
+            <CustomLinkButton text={'新規登録'} link={props.signUpUrl} />
+          </>
+        )}
       </Toolbar>
     </AppBar>
   );
