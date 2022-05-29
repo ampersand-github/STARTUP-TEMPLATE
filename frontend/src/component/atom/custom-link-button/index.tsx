030a06462756af602f8b1f59b99e0c44d6f21a37
ampersand-github <ampersand.dev@gmail.com>
2022-05-29 19:29:25 +0900

    custom-link-button

---
+++ frontend/src/component/atom/custom-link-button/index.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import {Typography} from "@mui/material";
+import React from "react";
+import Button from "@mui/material/Button";
+
+interface ICustomLinkButton {
+    text:string
+    link:string
+}
+
+export const CustomLinkButton = (props: ICustomLinkButton): JSX.Element => {
+    return (
+        <Button color="inherit">
+            <Link href={props.link}>
+                <Typography>{props.text}</Typography>
+            </Link>
+        </Button>
+);
+};
