c9df36d9fea4fee6663bef223726eefe956514d3
ampersand-github <ampersand.dev@gmail.com>
2022-05-31 23:13:19 +0900

    修正

--- frontend/src/component/template/base-layout/index.tsx
+++ frontend/src/component/template/base-layout/index.tsx
@@ -1,21 +1,37 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import { Container, Stack } from '@mui/material';
 import { Header, IHeader } from '../../organism/header';
 import { Footer } from '../../organism/footer';
+import { useAuthContext } from '../../../util/auth/auth-context';
+import { signOut } from '@firebase/auth';
+import { auth } from '../../../pages/_app';
+import router from 'next/router';
 
 export interface IBaseLayout {
   children: ReactNode;
-  header: IHeader;
 }
 
 export const BaseLayout = (props: IBaseLayout): JSX.Element => {
+  const { currentUser } = useAuthContext();
+  const [isAuthState, setAuthState] = useState<boolean>(false);
+  useEffect(() => {
+    setAuthState(!!currentUser);
+  }, [currentUser]);
+
+  const onSignOut = (): Promise<void> => {
+    return signOut(auth).then(() => {
+      console.log('onSignOut-ok');
+      router.push('');
+    });
+  };
+
   return (
     <Stack justifyContent="center">
       <Header
-        isAuth={props.header.isAuth}
-        onSignOut={props.header.onSignOut}
-        signInUrl={props.header.signInUrl}
-        signUpUrl={props.header.signUpUrl}
+        isAuth={isAuthState}
+        onSignOut={onSignOut}
+        signInUrl="/auth/sign-in"
+        signUpUrl="/auth/sign-up"
       />
       <Container
         component="main"
