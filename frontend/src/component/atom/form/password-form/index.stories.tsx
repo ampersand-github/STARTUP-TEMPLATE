bb6e7bf2f251fc0ac44c90f8dd81a57ce9649f07
ampersand-github <ampersand.dev@gmail.com>
2022-05-31 23:10:20 +0900

    password

---
+++ frontend/src/component/atom/form/password-form/index.stories.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { ComponentMeta } from '@storybook/react';
+import { PasswordForm } from './index';
+import { useForm } from 'react-hook-form';
+import { emailRule } from '../../../../util/validation-rule/email-rule';
+import { passwordRule } from '../../../../util/validation-rule/password-rule';
+
+export default {
+  title: 'Atom/Form/PasswordForm',
+  component: PasswordForm,
+} as ComponentMeta<typeof PasswordForm>;
+
+interface IDefault {
+  password: string;
+}
+export const Default = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IDefault>();
+  const onSubmit = (data: any) => console.log(data);
+  console.log(errors);
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <PasswordForm
+        control={control}
+        rules={passwordRule}
+        name={'password'}
+        label={'パスワード'}
+        isError={errors.password !== undefined}
+        errorText={errors.password?.message}
+      />
+      <input type="submit" />
+    </form>
+  );
+};
---
+++ frontend/src/component/atom/form/text-form/index.tsx
@@ -0,0 +1,41 @@
+import { TextField } from '@mui/material';
+import React from 'react';
+import { useController, Control } from 'react-hook-form';
+
+// https://www.w3schools.com/tags/att_input_type.asp
+type InputType = 'email' | 'text' | 'password';
+
+interface ITextForm {
+  control: Control<any>;
+  name: string;
+  label: string;
+  rules: any;
+  isError: boolean;
+  errorText: string | undefined;
+  defaultValue?: string;
+  placeholder?: string;
+  inputType: InputType;
+}
+
+export const TextForm = (props: ITextForm): JSX.Element => {
+  const { field } = useController({
+    control: props.control,
+    name: props.name,
+    rules: props.rules,
+  });
+
+  return (
+    <TextField
+      id={field.name} //id="password"
+      name={field.name}
+      label={props.label}
+      type={props.inputType}
+      onChange={field.onChange}
+      error={props.isError}
+      helperText={props.errorText}
+      value={field.value}
+      defaultValue={props.defaultValue}
+      placeholder={props.placeholder}
+    />
+  );
+};
