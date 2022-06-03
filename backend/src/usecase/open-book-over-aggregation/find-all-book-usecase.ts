ebc5b3305ad2aa8c3dff377f0ba3e7d084c49e38
ampersand-github <ampersand.dev@gmail.com>
2022-06-04 01:13:22 +0900

    formmater

--- backend/src/usecase/open-book-over-aggregation/find-all-book-usecase.ts
+++ backend/src/usecase/open-book-over-aggregation/find-all-book-usecase.ts
@@ -1,12 +1,13 @@
 import { IOpenBookOAQS } from './__interface__/open-book-OAQS-interface';
-import { OpenBookId } from '../../domain/open-book/open-book-id/open-book-id';
 import { OneBookOutputDTO } from './__dto__/one-book-output-DTO';
 import { IOpenBookOA } from './__interface__/open-book-OA-interface';
 
 export interface IFindAllBookUC {
   openBookOAQS: IOpenBookOAQS;
 }
-export const findAllBookUC = async (props: IFindAllBookUC): Promise<OneBookOutputDTO[]> => {
+export const findAllBookUC = async (
+  props: IFindAllBookUC,
+): Promise<OneBookOutputDTO[]> => {
   const all = await props.openBookOAQS.findAll();
   return all.map((one: IOpenBookOA) => new OneBookOutputDTO(one));
 };
