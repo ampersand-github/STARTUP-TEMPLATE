5b6e138e554a281a9712449933733bc70abc6b71
ampersand-github <ampersand.dev@gmail.com>
2022-06-12 13:58:25 +0900

    url

--- frontend/src/service/url.ts
+++ frontend/src/service/url.ts
@@ -4,3 +4,4 @@ export const SIGN_UP_URL = '/auth/sign-up';
 export const PAYMENT_URL = '/payment';
 export const STRIPE_URL = '/stripe';
 export const STRIPE_PAYMENT_URL = '/stripe/payment';
+export const MOCK_BOOK_URL = '/mock-book';
