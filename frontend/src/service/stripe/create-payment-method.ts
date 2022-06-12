c9d548cd32f96b424c82ebf3aec23d496a31a5d6
ampersand-github <ampersand.dev@gmail.com>
2022-06-12 14:02:33 +0900

    iroiro

--- frontend/src/service/stripe/create-payment-method.ts
+++ frontend/src/service/stripe/create-payment-method.ts
@@ -1,6 +1,6 @@
 import { PaymentMethodResult } from '@stripe/stripe-js/types/stripe-js/stripe';
 import * as stripeJs from '@stripe/stripe-js';
-import { StripeCardElement, StripeElements } from '@stripe/stripe-js';
+import { StripeCardElement } from '@stripe/stripe-js';
 
 export interface ICreatePaymentMethod {
   stripe: stripeJs.Stripe;
